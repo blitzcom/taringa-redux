2806425300ddc4c5bc13f2b004bfe5d7002063f1
Diego Lopez <lgdiego@outlook.com>
2021-03-20 19:13:06 -0600

    Configure sass in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,24 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+          modules: {
+            localIdentName: '[local]--[hash:base64:5]',
+          },
+        },
+        sassLoaderOptions: {
+          sassOptions: {
+            includePaths: [path.join(__dirname, '../src/sass')],
+          },
+        },
+      },
+    },
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    'storybook-css-modules-preset',
   ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '../src/sass/taringa.scss';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
