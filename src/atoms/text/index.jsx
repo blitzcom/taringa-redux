cb2f2d04747258c290579b78fec311bbb5e2507a
Diego Lopez <lgdiego@outlook.com>
2021-03-24 15:22:39 -0600

    Add primary and secondary variants to text atom

--- src/atoms/text/index.jsx
+++ src/atoms/text/index.jsx
@@ -3,9 +3,13 @@ import classNames from 'classnames';
 
 import style from './style.module.scss';
 
-function Text({ children, truncate }) {
+function Text({ children, truncate, variant }) {
   return (
-    <p className={classNames(style.text, { [style.truncate]: truncate })}>
+    <p
+      className={classNames(style.text, style[variant], {
+        [style.truncate]: truncate,
+      })}
+    >
       {children}
     </p>
   );
@@ -14,10 +18,12 @@ function Text({ children, truncate }) {
 Text.propTypes = {
   children: PropTypes.node.isRequired,
   truncate: PropTypes.bool,
+  variant: PropTypes.oneOf(['primary', 'secondary']),
 };
 
 Text.defaultProps = {
   truncate: false,
+  variant: 'primary',
 };
 
 export default Text;
