91a31ac0f9217656d238925bcdc165373349de6c
Diego Lopez <lgdiego@outlook.com>
2021-03-24 18:17:13 -0600

    Add size prop to text atom

--- src/atoms/text/index.jsx
+++ src/atoms/text/index.jsx
@@ -3,10 +3,10 @@ import classNames from 'classnames';
 
 import style from './style.module.scss';
 
-function Text({ children, truncate, variant }) {
+function Text({ children, truncate, variant, size }) {
   return (
     <p
-      className={classNames(style.text, style[variant], {
+      className={classNames(style.text, style[variant], style[size], {
         [style.truncate]: truncate,
       })}
     >
@@ -19,11 +19,13 @@ Text.propTypes = {
   children: PropTypes.node.isRequired,
   truncate: PropTypes.bool,
   variant: PropTypes.oneOf(['primary', 'secondary']),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
 };
 
 Text.defaultProps = {
   truncate: false,
   variant: 'primary',
+  size: 'medium',
 };
 
 export default Text;
