1f6680c4210fa4541ead39c6618586147a0c7ee7
Diego Lopez <lgdiego@outlook.com>
2021-03-27 13:36:24 -0600

    Add sass hash var names in prod

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const path = require('path');
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/sass')],
+    additionalData: `
+      $production: ${process.env.NODE_ENV === 'production'};
+    `,
   },
 };
